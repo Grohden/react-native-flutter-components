fa0c2f4a3d99776ed774dc32247b958aa5345e85
Gabriel Rohden <gabriel_rohden@outlook.com>
2023-08-31 23:52:19 -0300

    feat: add support for lib alias

--- example/babel.config.js
+++ example/babel.config.js
@@ -14,6 +14,7 @@ module.exports = function (api) {
           alias: {
             // For development, we want to alias the library to the source
             [pak.name]: path.join(__dirname, '..', pak.source),
+            '@lib': path.join(__dirname, '..', 'src'),
           },
         },
       ],
