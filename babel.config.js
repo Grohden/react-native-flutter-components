756ddbc716b7ef82de4c9e9a7c9ec1903daf84e1
Gabriel Rohden <gabriel_rohden@outlook.com>
2023-09-01 00:59:05 -0300

    chore: reformat root files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
+const path = require('path');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+        alias: {
+          '@lib': path.join(__dirname, '.', 'src'),
+        },
+      },
+    ],
+  ],
 };
