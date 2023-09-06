dc2809018c88ed555f0fe9c2e639e230048d29d8
Gabriel Rohden <gabriel_rohden@outlook.com>
2023-09-06 00:05:50 -0300

    fix: expanded behavior like flutter one

--- src/components/atoms/containers/Expanded.tsx
+++ src/components/atoms/containers/Expanded.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-export const Expanded = ({ children }: { children?: React.ReactNode }) => (
-  <View style={styles.expanded}>{children}</View>
+export const Expanded = ({ children }: { children?: React.ReactChild }) => (
+  <View style={styles.expanded}>
+    {children && Children.only(children)}
+  </View>
 );
 
 export const styles = StyleSheet.create({
