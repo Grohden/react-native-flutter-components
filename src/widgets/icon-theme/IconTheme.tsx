dfba64371490d92e7798c15a03aa7c40d754207e
Gabriel Rohden <gabriel_rohden@outlook.com>
2023-09-19 22:52:56 -0300

    refactor: use flutter like api for icon theme

--- src/widgets/icon-theme/IconTheme.tsx
+++ src/widgets/icon-theme/IconTheme.tsx
@@ -1,12 +1,39 @@
-import { createComponentContext } from '@lib/context';
-
 import { IconThemeData } from '@lib/widgets/icon-theme-data';
+import React, { createContext, useContext } from 'react';
 
-export const {
-  Provider: IconTheme,
-  useComponentContext: useIconTheme,
-} = createComponentContext(
-  IconThemeData({
-    size: 24,
-  }),
+const Context = createContext<IconThemeData | null>(IconThemeData({
+  size: 24,
+}));
+
+export const useIconTheme = () => {
+  const theme = useContext(Context);
+
+  if (!theme) {
+    throw new Error('IconTheme context is required');
+  }
+
+  return theme;
+};
+
+export const IconTheme = (
+  props: {
+    data: IconThemeData;
+    children: React.ReactChild;
+  },
+) => (
+  <Context.Provider value={props.data}>
+    {props.children}
+  </Context.Provider>
 );
+
+IconTheme.merge = (
+  props: {
+    data: IconThemeData;
+    children: React.ReactChild;
+  },
+) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const data = useIconTheme();
+
+  return <IconTheme data={data.merge(props.data)}>{props.children}</IconTheme>;
+};
