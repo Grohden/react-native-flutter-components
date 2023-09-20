05e553d700b95866624bc5f2631c96162d562a98
Gabriel Rohden <gabriel_rohden@outlook.com>
2023-09-19 22:54:51 -0300

    refactor: more api for icon theme data

--- example/src/implementations/Icon.tsx
+++ example/src/implementations/Icon.tsx
@@ -1,21 +1,36 @@
 import { MaterialIcons } from '@expo/vector-icons';
-import { useIconTheme } from '@lib/widgets';
+import {
+  SizedBox,
+  useIconTheme,
+} from '@grohden/react-native-flutter-components';
+import type { Color, Shadow } from '@grohden/react-native-flutter-components';
 import React from 'react';
 
-export const Icon = (
-  { name, size, color }: {
-    name: React.ComponentProps<typeof MaterialIcons>['name'];
-    size?: number;
-    color?: string;
-  },
-) => {
-  const theme = useIconTheme();
+export const Icon = (props: {
+  name: React.ComponentProps<typeof MaterialIcons>['name'];
+  size?: number;
+  color?: Color;
+  fill?: number;
+  opticalSize?: number;
+  weight?: number;
+  grade?: number;
+  shadows?: Shadow[];
+}) => {
+  const iconTheme = useIconTheme();
+  const iconSize = props.size ?? iconTheme.size;
+  // const iconFill = props.fill ?? iconTheme.fill;
+  // const iconWeight = props.weight ?? iconTheme.weight;
+  // const iconGrade = props.grade ?? iconTheme.grade;
+  // const iconOpticalSize = props.opticalSize ?? iconTheme.opticalSize;
+  // const iconShadows = props.shadows ?? iconTheme.shadows;
 
   return (
-    <MaterialIcons
-      name={name}
-      size={size ?? theme.size}
-      color={color ?? theme.color?.hex()}
-    />
+    <SizedBox width={iconSize} height={iconSize}>
+      <MaterialIcons
+        name={props.name}
+        size={iconSize}
+        color={(props.color || iconTheme.color)?.hex()}
+      />
+    </SizedBox>
   );
 };
--- src/widgets/icon-theme-data/IconThemeData.ts
+++ src/widgets/icon-theme-data/IconThemeData.ts
@@ -1,10 +1,68 @@
-import type { Color } from '@lib/std-ui/color';
+import { Color } from '@lib/std-ui/color';
+import type { Shadow } from '@lib/std-ui/shadow';
+import { clampDouble } from '@material/material-color-utilities';
 
-export type IconThemeData = {
+type IconThemeDataProps = {
   size?: number;
   color?: Color;
+  fill?: number;
+  grade?: number;
+  opacity?: number;
+  opticalSize?: number;
+  weight?: number;
+  shadows?: Shadow[];
 };
 
-export const IconThemeData = (props: IconThemeData) => {
-  return props;
+export type IconThemeData = IconThemeDataProps & {
+  copyWith(data: IconThemeDataProps): IconThemeData;
+  merge(other: IconThemeDataProps | undefined): IconThemeData;
 };
+
+export const IconThemeData = (
+  { opacity, ...props }: IconThemeDataProps,
+): IconThemeData => ({
+  ...props,
+  get opacity() {
+    return !opacity ? undefined : clampDouble(0.0, 1.0, opacity);
+  },
+  copyWith(other) {
+    return IconThemeData({
+      size: other.size ?? this.size,
+      fill: other.fill ?? this.fill,
+      weight: other.weight ?? this.weight,
+      grade: other.grade ?? this.grade,
+      opticalSize: other.opticalSize ?? this.opticalSize,
+      color: other.color || this.color,
+      opacity: other.opacity ?? this.opacity,
+      shadows: other.shadows || this.shadows,
+    });
+  },
+  merge(other) {
+    if (other == null) {
+      return this;
+    }
+
+    return this.copyWith({
+      size: other.size,
+      fill: other.fill,
+      weight: other.weight,
+      grade: other.grade,
+      opticalSize: other.opticalSize,
+      color: other.color,
+      opacity: other.opacity,
+      shadows: other.shadows,
+    });
+  },
+});
+
+IconThemeData.fallback = () =>
+  IconThemeData({
+    size: 24.0,
+    fill: 0.0,
+    weight: 400.0,
+    grade: 0.0,
+    opticalSize: 48.0,
+    color: Color('000000FF'),
+    opacity: 1.0,
+    shadows: undefined,
+  });
